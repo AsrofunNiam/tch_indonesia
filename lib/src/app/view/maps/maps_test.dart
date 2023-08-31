47841d472b42dc282cf62168135ca87703af4227
asrofun-niam <asrofunniam44@gmail.com>
2023-09-01 05:55:28 +0700

    add maps route

--- lib/src/app/view/maps/maps_test.dart
+++ lib/src/app/view/maps/maps_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsTest extends StatefulWidget {
@@ -12,33 +13,71 @@ class MapsTest extends StatefulWidget {
 
 class _MapsTestState extends State<MapsTest> {
   Completer<GoogleMapController> _createState = Completer();
+  String googleKey = 'AIzaSyCICwChYERrvs8w6RUSXv-Ad9H82uZkcKU';
   final CameraPosition _kGoolePlex = const CameraPosition(
-      target: LatLng(37.4233223322, -122.0433434), zoom: 14.4746);
+      target: LatLng(-6.244392, 106.776544), zoom: 14.4746);
 
   final Marker _kGooglePlexMarker = const Marker(
     markerId: MarkerId('_kGooglePlex'),
     infoWindow: InfoWindow(title: 'Google Test'),
     icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(37.4233223322, -122.0433434),
+    position: LatLng(-6.244392, 106.776544),
+  );
+  final Marker _destination1Marker = const Marker(
+    markerId: MarkerId('_kGooglePlex'),
+    infoWindow: InfoWindow(title: 'Google Test'),
+    icon: BitmapDescriptor.defaultMarker,
+    position: LatLng(-6.254591, 106.75719),
   );
 
-  final LatLng sourceLocation = const LatLng(106.7196769, -6.2839295);
-  final LatLng sourceDestination = const LatLng(37.334229383, -122.006600055);
+  final Marker _destination2Marker = const Marker(
+    markerId: MarkerId('_kGooglePlex'),
+    infoWindow: InfoWindow(title: 'Google Test'),
+    icon: BitmapDescriptor.defaultMarker,
+    position: LatLng(-6.234397, 106.678446),
+  );
+
+  List<LatLng> polyLineCoordinate = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleKey,
+      PointLatLng(-6.244392, 106.776544),
+      PointLatLng(-6.244392, 106.776544),
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((element) =>
+          polyLineCoordinate.add(LatLng(element.latitude, element.longitude)));
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        markers: {_kGooglePlexMarker},
+        markers: {_kGooglePlexMarker, _destination1Marker, _destination2Marker},
         initialCameraPosition: _kGoolePlex,
         onMapCreated: (controller) {
           _createState.complete(controller);
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.directions_boat),
+        polylines: {
+          Polyline(
+              polylineId: PolylineId('route'),
+              points: polyLineCoordinate,
+              color: Colors.blue,
+              width: 4)
+        },
       ),
     );
   }
